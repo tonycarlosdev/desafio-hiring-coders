f9f9b25e53cdc6357348b30a9b4bea842798c5fa
Tony Carlos <tonycarlosdev@gmail.com>
2021-07-17 00:09:43 -0300

    Meu primeiro commit após inicialização

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let nome = document.getElementById('nome').value;
+  let email = document.getElementById('email').value;
+  let data = {
+    nome,
+    email,
+  }
+  let convertData = JSON.stringify(data);
+
+  localStorage.setItem('lead', convertData);
+
+  let content = document.getElementById('content');
+
+  let carregando = `<p>carregando...</p>`
+
+  let pronto = `Parabens! ${nome}, PROMOÇÃO chegando em breve!`
+
+  content.innerHTML = carregando
+  
+
+  setTimeout(() => {
+    content.innerHTML = pronto;
+    alert('Cadastrado com sucesso!')
+  },1000)
+  
+})
